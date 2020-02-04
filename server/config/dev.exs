aecf150841cec2d8d71a8d523baa75f3a097d0bc
Arman Yaraee <arman@yaraee.net>
2020-02-04 21:22:09 +0000

    ignored secret file and fixed db

--- server/config/dev.exs
+++ server/config/dev.exs
@@ -4,7 +4,7 @@ use Mix.Config
 config :server, Server.Repo,
   username: "postgres",
   password: "postgres",
-  database: "server_dev",
+  database: "fileserver_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
