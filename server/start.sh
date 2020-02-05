e57fd167d128f3a4ddfbfac1e71d00ea008b2196
Arman Yaraee <arman@yaraee.net>
2020-02-05 17:04:37 +0000

    Add startup files

---
+++ server/start.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+while ! pg_isready -q -h $PGHOST -p $PGPORT -U $PGUSER
+do
+  echo "$(date) - waiting for database to start"
+  sleep 2
+done
+
+# Create database if it doesn't exist.
+if [[ -z `psql -Atqc "\\list $PGDATABASE"` ]]; then
+  echo "Database $PGDATABASE does not exist. Creating..."
+  mix ecto.create
+  echo "Database $PGDATABASE created."
+fi
+
+mix ecto.migrate
+
+exec mix phx.server
