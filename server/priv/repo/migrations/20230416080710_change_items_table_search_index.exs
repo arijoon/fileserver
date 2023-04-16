035458b7c26e6b579dfeb65c86dfb02c6cdce485
Arman Yaraee <arman@yaraee.net>
2023-04-16 14:14:32 +0100

    Fix the database migration

--- server/priv/repo/migrations/20230416080710_change_items_table_search_index.exs
+++ server/priv/repo/migrations/20230416080710_change_items_table_search_index.exs
@@ -5,11 +5,12 @@ defmodule Server.Repo.Migrations.ChangeItemsTableSearchIndex do
     execute "CREATE EXTENSION IF NOT EXISTS pg_trgm;"
     execute "CREATE INDEX items_path_gin ON items USING gin (path gin_trgm_ops);"
     execute "ALTER TABLE items ADD COLUMN path_vec tsvector GENERATED ALWAYS AS (to_tsvector('english', translate(path, '/.', ' '))) STORED;"
-    execute "CREATE INDEX path_vec ON items USING gin (path_vec gin_trgm_ops);"
+    execute "CREATE INDEX items_path_vec ON items USING gin (path_vec);"
   end
 
   def rollback do
     execute "DROP INDEX items_path_gin"
-    execute "ALTER TABLE table_name DROP COLUMN path_vec;"
+    execute "DROP INDEX items_path_vec"
+    execute "ALTER TABLE items DROP COLUMN path_vec;"
   end
 end
